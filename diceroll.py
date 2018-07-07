c66568dec2de1383e1f632b3f521bf9a4fe0ddb8
sirobeco <sirobeco@gmail.com>
2018-07-07 18:33:22 +0900

    Greetings added

--- diceroll.py
+++ diceroll.py
@@ -1,4 +1,7 @@
 import numpy as np
+print("What is your name?")
+name = input()
+print("Hello, %s!"%name)
 dice = np.random.randint(1,6,2)
 for i in range(2):print("Die%d:"%int(i+1),dice[i])
 print("Total value:",sum(dice))
