fcaa67e1fb85346789cc745b649dfb96236119cc
sirobeco <sirobeco@gmail.com>
2018-07-07 19:14:43 +0900

    Won/lost message modified

--- diceroll.py
+++ diceroll.py
@@ -6,6 +6,6 @@ dice = np.random.randint(1,6,2)
 for i in range(2):print("Die%d:"%int(i+1),dice[i])
 print("Total value:",sum(dice))
 if sum(dice)>7:
-    print("You won")
+    print("%s won"%name)
 else:
-    print("You lost")
+    print("%s lost"%name)
